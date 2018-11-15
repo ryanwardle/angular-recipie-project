77280d1a3f9adf54ba34807eeb1ee377d27a7123
ryanwardle <wardle2106@gmail.com>
2018-11-15 15:28:28 -0500

    update

--- src/app/recipies/recipie-list/recipie-list.component.ts
+++ src/app/recipies/recipie-list/recipie-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipie } from '../recipie.model';
 @Component({
   selector: 'app-recipie-list',
   templateUrl: './recipie-list.component.html',
   styleUrls: ['./recipie-list.component.css']
 })
 export class RecipieListComponent implements OnInit {
-
+  recipies: Recipie[] = [
+    new Recipie('Test recipie', 'this is a test',
+    `https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05
+    /frying-pan-pizza-easy-recipe-collection.jpg`)
+  ];
   constructor() { }
 
   ngOnInit() {
